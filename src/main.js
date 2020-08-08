abc0cc3b92a05b750b34787a7dd039570f17dfb4
Lozovaya-collab <66836372+Lozovaya-collab@users.noreply.github.com>
2020-08-09 00:58:29 +0400

    first commit

---
+++ src/main.js
@@ -0,0 +1,147 @@
+let carouselSliders
+let slidersImg
+const prevButton = document.querySelector('#prev')
+const nextButton = document.querySelector('#next')
+const swipeButton = document.querySelector('#swipe')
+let size
+let slideInterval
+let obj
+let counter = 0
+let transform = 0
+let moving = false
+let initialPosition = null
+
+function SliderJS(sliderID, arguments){
+    
+    obj = arguments
+    carouselSliders = document.querySelector(sliderID)
+    slidersImg = document.querySelectorAll(sliderID +' img')
+    
+    size = slidersImg[0].clientWidth
+    slideInterval = setInterval(nextSlide, obj.autoplayInterval)
+    carouselSliders.addEventListener('transitionend', ()=>{
+    
+        if (slidersImg[counter].id === 'lastSlide'){
+            carouselSliders.style.transition = "none"
+            counter = slidersImg.length - 2
+            carouselSliders.style.transform = 'translateX(' + (-size * counter) + 'px)'
+        } else if (slidersImg[counter].id === 'firstSlide'){
+            carouselSliders.style.transition = "none"
+            counter = slidersImg.length - counter
+            carouselSliders.style.transform = 'translateX(' + (-size * counter) + 'px)'
+        }
+    
+        
+
+    })
+    
+    if (obj.hideControls === false) {
+        prevButton.style.display = 'none'
+        nextButton.style.display = 'none'
+        swipeButton.style.display = 'none'
+    }
+    
+    nextButton.addEventListener('click', ()=>{
+        nextSlide()
+        if (obj.playing){
+            clearInterval(slideInterval)
+            slideInterval = setInterval(nextSlide,obj.autoplayInterval)
+        } 
+    })
+    
+    prevButton.addEventListener('click', ()=>{
+        prevSlide() 
+        if (obj.playing){
+            clearInterval(slideInterval)
+            slideInterval = setInterval(nextSlide,obj.autoplayInterval)
+        } 
+        
+    })
+    
+    swipeButton.addEventListener('click', ()=>{
+        console.log(obj.playing);
+        if(obj.playing){
+            console.log('pause')
+            pauseShow()
+        }
+        else{
+            playShow()
+        }
+        
+    })
+    console.log('1')
+    window.addEventListener('mousedown', gestureStart)
+    window.addEventListener('mousemove', gestureMove)
+    window.addEventListener('mouseup', gestureEnd)
+    
+    console.log('2');
+    window.addEventListener('touchstart', gestureStart)
+    window.addEventListener('touchmove', gestureMove)
+    window.addEventListener('touchend', gestureEnd)
+         
+}
+    
+
+
+function nextSlide(){
+    if(counter >= slidersImg.length - 1) return
+    carouselSliders.style.transition = "transform 1s ease-in-out"
+    counter++
+    carouselSliders.style.transform = 'translateX(' + (-size * counter) + 'px)'
+}
+function prevSlide(){
+    if(counter <= 0) return
+    carouselSliders.style.transition = "transform 1s ease-in-out"
+    counter--
+    carouselSliders.style.transform = 'translateX(' + (-size * counter) + 'px)'
+}
+
+
+function pauseShow() {
+    swipeButton.style.color = "red"
+    obj.playing = false;
+    clearInterval(slideInterval);
+}
+ 
+function playShow() {
+    swipeButton.style.color = "black"
+    obj.playing = true;
+    slideInterval = setInterval(nextSlide,obj.autoplayInterval);
+}
+
+function gestureStart(event){
+    console.log('3');
+    
+    initialPosition = event.pageX
+    initialPosition = event.pageX
+    console.log(initialPosition);
+    moving = true
+    const transformMatrix = window.getComputedStyle(carouselSliders).getPropertyValue('transform')
+    console.log(transformMatrix)
+    if (transformMatrix != 'none'){
+        transform = parseInt(transformMatrix.split(',')[4].trim())
+    }
+    
+    carouselSliders.ondragstart = function() {
+        return false;
+      };
+}
+
+function gestureMove(event){
+    if (moving){
+        console.log('4');
+        const currentPosition = event.pageX
+        console.log(currentPosition);
+        const difference = currentPosition - initialPosition
+        console.log(transform + difference);
+        carouselSliders.style.transform = 'translateX(' + (transform + difference) + 'px)'
+    }
+}
+
+function gestureEnd(event){
+    console.log('5');
+    moving = false
+    initialPosition = 0
+    
+    
+}
