b8a19a3fc0f59cf65ea3e308a5cb72f1b621a6b3
Lozovaya-collab <66836372+Lozovaya-collab@users.noreply.github.com>
2020-08-10 01:35:19 +0400

    commit5

--- src/main.js
+++ src/main.js
@@ -1,56 +1,72 @@
 function SliderJS(sliderID, arguments){
-    console.log('function');
-    let prevButton = document.createElement("button");
-    prevButton.innerText = '<';
-    document.body.appendChild(prevButton);
-    let playButton = document.createElement("button");
-    playButton.innerText = '||';
-    document.body.appendChild(playButton);
-    let nextButton = document.createElement("button");
-    nextButton.innerText = '>';
-    document.body.appendChild(nextButton);
-    let carouselSliders = document.querySelector(sliderID)
-    let slidersImg = document.querySelectorAll(sliderID +' img')
-    let size = slidersImg[0].clientWidth
-    let obj = arguments
-    let counter = 0
     
-    
-    if (obj.hideControls === false) {
-        prevButton.style.display = 'none'
-        nextButton.style.display = 'none'
-        playButton.style.display = 'none'
-    }
-    
-    prevButton.addEventListener('click', ()=>{
-        console.log('prevButton');
-        if(counter <= 0){
-            slidersImg[counter].style.display = 'none'
-            counter = slidersImg.length - 1
-            slidersImg[counter].style.display = 'block'
-            return
-        } 
-        slidersImg[counter].style.display = 'none'
-        counter--
-        slidersImg[counter].style.display = 'block'
+        console.log('function');
+        let prevButton = document.createElement("button");
+        prevButton.innerText = '<';
+        document.body.appendChild(prevButton);
+        let playButton = document.createElement("button");
+        playButton.innerText = '||';
+        document.body.appendChild(playButton);
+        let nextButton = document.createElement("button");
+        nextButton.innerText = '>';
+        document.body.appendChild(nextButton);
+        let carouselSliders = document.querySelector(sliderID)
+        let slidersImg = document.querySelectorAll(sliderID +' img')
+        let size = slidersImg[0].clientWidth
+        let obj = arguments
+        let counter = 0
         
         
         
-    })
-    playButton.addEventListener('click', ()=>{
-        console.log('playButton');
-        
-    })
-    nextButton.addEventListener('click', ()=>{
-        console.log('nextButton');
-        if(counter >= slidersImg.length - 1){
-            slidersImg[counter].style.display = 'none'
-            counter = 0
-            slidersImg[counter].style.display = 'block'
-            return
+        function otherSlidesNone(){
+            for(let i = 0; i< slidersImg.length; i++){
+                slidersImg[i].style.display = 'none'
+            }    
         }
-        slidersImg[counter].style.display = 'none'
-        counter++
-        slidersImg[counter].style.display = 'block'
-    })
+        function currentSlideBlock(n){
+            
+            slidersImg[n].style.display = 'block'
+            
+            
+        }
+        otherSlidesNone()
+        currentSlideBlock(counter) 
+        if (obj.hideControls === false) {
+            prevButton.style.display = 'none'
+            nextButton.style.display = 'none'
+            playButton.style.display = 'none'
+        }
+        
+        prevButton.addEventListener('click', ()=>{
+            console.log('prevButton');
+            if(counter <= 0){
+                otherSlidesNone()
+                counter = slidersImg.length - 1
+                currentSlideBlock(counter)
+                return
+            } 
+            otherSlidesNone()
+            counter--
+            currentSlideBlock(counter)
+            
+            
+            
+        })
+        playButton.addEventListener('click', ()=>{
+            console.log('playButton');
+            
+        })
+        nextButton.addEventListener('click', ()=>{
+            console.log('nextButton');
+            if(counter >= slidersImg.length - 1){
+                otherSlidesNone()
+                counter = 0
+                currentSlideBlock(counter)
+                return
+            }
+            otherSlidesNone()
+            counter++
+            currentSlideBlock(counter)
+        })
+    
 }
